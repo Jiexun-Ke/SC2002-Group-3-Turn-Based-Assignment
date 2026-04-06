30e0f18c797034647d9082171b43c529b8c03198
IFOO <isaacfoojx@gmail.com>
2026-04-06 12:50:59 +0800

    Create GameManager.java

---
+++ src/control/GameManager.java
@@ -0,0 +1,8 @@
+package control;
+
+public class GameManager {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
