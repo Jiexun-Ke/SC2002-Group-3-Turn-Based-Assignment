4deb93d888ccd74d1b95256f20334e1e8d50806b
Evelyn <lai1si2zhu@gmail.com>
2026-04-09 12:33:11 +0800

    Update GameUI.java

--- src/boundary/GameUI.java
+++ src/boundary/GameUI.java
@@ -1,7 +1,7 @@
 package boundary;
 
 import java.util.List;
-
+import model.items.*;
 import model.combatants.*;
 
 public class GameUI {
@@ -18,6 +18,15 @@ public class GameUI {
 
     //MENUS & INPUTS (RETURNING DATA TO CONTROL)
 
+    public int showMainMenu(){
+        print("==================================");
+        print("       TURN-BASED COMBAT RPG      ");
+        print("==================================");
+        print("1. Start New Game");
+        print("2. Exit");
+
+        return validator.getIntInRange("Select an option: ", 1, 2);
+    }
 
     public int promptPlayerSelection(){
         print("==================================");
@@ -38,10 +47,11 @@ public class GameUI {
     public int promptPlayerAction(Player player){
         print("--- IT IS YOUR TURN ---");
         print("1. Attack");
-        print("2. Use Skill");
+        print("2. Defend");
+        print("3. Use Skill");
         print("3. Use Item");
 
-        return validator.getIntInRange("Choose your action (1-3): ",1, 3 );
+        return validator.getIntInRange("Choose your action (1-4): ",1, 4 );
     }
 
     public int promptEnemyTargetSelection(List<Enemy> enemies){
@@ -49,17 +59,34 @@ public class GameUI {
         for (int i = 0; i < enemies.size(); i++){
             Enemy e = enemies.get(i);
             if (e.isAlive()){
-                print((i + 1) + ". " + e.getName() + e.getCurrentHP() + "/" + e.getMaxHP() + ")");
+                System.out.println((i + 1) + ". " + e.getName() + e.getCurrentHP() + "/" + e.getMaxHP() + ")");
             }
         }
         return validator.getIntInRange("Choose target: ", 1, enemies.size());
     }
 
+    public int promptItemSelection(Item[] inventory){
+        print("--- INVENTORY ---");
 
+        int availItems = 0;
+        for (int i = 0; i < inventory.length; i++){
+            if (inventory[i] != null){
+                System.out.println((i+1) +". " + inventory[i].getName() + " - " + inventory[i].getDescription());
+                availItems++;
+            }
+        }
+
+        if(availItems == 0){
+            print("Your inventory is empty.");
+            return -1;
+        }
+
+        System.out.println((availItems + 1) + ". Cancel");
+        return validator.getIntInRange("Choose an item: ", 1, availItems + 1);
+    }
+   
+   
     // DISPLAY OUTPUTS (DATA COLLECTED FROM CONTROL)
-
-
-
 
     public void showVictory(int remainingHp, int maxHp, int rounds) {
         print("----------- VICTORY ------------");
@@ -76,22 +103,22 @@ public class GameUI {
     }
 
     public void displayRoundInfo(int round, Player player, Enemy[] enemies) {
-        print("Player HP: " + player.getCurrentHP() + "/" + player.getMaxHP());
+        System.out.println("Player HP: " + player.getCurrentHP() + "/" + player.getMaxHP());
         print("--- Enemies ---");
         for (Enemy enemy : enemies) {
             if (enemy.isAlive()) {
-                print("- " + enemy.getName() + " HP: " + enemy.getCurrentHP() + "/" + enemy.getMaxHP());
+                System.out.println("- " + enemy.getName() + " HP: " + enemy.getCurrentHP() + "/" + enemy.getMaxHP());
             }
         }
-        print("------------------------------")
+        print("------------------------------");
     }
 
     public void showNewWave(int enemyCount) {
-        print("WARNING: A new wave of enemies approaches! ("+ enemyCount + "enemies)");
+        System.out.println("WARNING: A new wave of enemies approaches! ("+ enemyCount + "enemies)");
     }
 
     public void showMessage(String message) {
-    print(message);
+    System.out.println(message);
     }
 
     public void showRoundHeader(int roundNumber) {
