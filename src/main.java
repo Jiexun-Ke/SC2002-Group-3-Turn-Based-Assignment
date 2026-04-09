b1a77d3f510a13a2c28a03bd5b2d35a2028c5736
Evelyn <lai1si2zhu@gmail.com>
2026-04-09 13:16:34 +0800

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,3 +1,33 @@
-public class main {
+import boundary.GameUI;
+import control.SetupController;
+import control.GameController;
 
+public class Main {
+    public static void main(String[] args){
+        GameUI ui = new GameUI();
+
+        int choice = ui.showMainMenu();
+        if(choice == 2){
+            ui.showMessage("Exiting the Arena. Goodbye!");
+            ui.closeUI();
+            return;
+        }
+
+        SetupController setup = new SetupController(ui);
+        
+        try {
+            GameController game = setup.createGame();
+
+            if (game != null){
+                game.startBattle();
+            }
+        }
+        catch (UnsupportedOperationException e){
+            ui.showMessage("Finish game controllers before game commence");
+            ui.showMessage("Error details: " + e.getMessage());
+
+        }
+
+        ui.closeUI();
+    }
 }
