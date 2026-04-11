54d4f47cc5c23b115b5a1ff5269512a5ffd15cdf
IFOO <isaacfoojx@gmail.com>
2026-04-11 21:07:02 +0800

    Update SetupController.java

--- src/control/SetupController.java
+++ src/control/SetupController.java
@@ -2,10 +2,15 @@ package control;
 
 
 import boundary.GameUI;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import model.combatants.Enemy;
+import model.combatants.Goblin;
 import model.combatants.Player;
+import model.combatants.Wolf;
 import model.turn_order.SpeedBasedTurnStrategy;
 import model.turn_order.TurnOrderStrategy;
 
@@ -43,11 +48,34 @@ public class SetupController {
         return null;
     }
 
-    private Queue<List<Enemy>> createBackupWaves() {
-        
-        throw new UnsupportedOperationException("Unimplemented method 'createBackupWaves'");
-    }
-}   
+    private Queue<List<Enemy>> createBackupWaves(int difficulty) {
+        Queue<List<Enemy>> remainingWaves = new LinkedList<>();
+
+        switch (difficulty) {
+            case 1: // Easy
+                break;
+
+            case 2: // Medium
+                List<Enemy> mediumBackup = new ArrayList<>();
+                mediumBackup.add(new Wolf());
+                mediumBackup.add(new Wolf());
+                remainingWaves.add(mediumBackup);
+                break;
+
+            case 3: // Hard
+                List<Enemy> hardBackup = new ArrayList<>();
+                hardBackup.add(new Goblin());
+                hardBackup.add(new Wolf());
+                hardBackup.add(new Wolf());
+                remainingWaves.add(hardBackup);
+                break;
+            
+            default:
+                throw new IllegalArgumentException("Invalid difficulty");
+            }
+
+    return remainingWaves;
+    }   
 
 
 
