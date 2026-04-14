f88325ec9bd0685ccecb8c6946707ee3336622c5
JX <kjiexun@gmail.com>
2026-04-15 02:20:17 +0800

    Update GameController.java

--- src/control/GameController.java
+++ src/control/GameController.java
@@ -14,6 +14,7 @@ import model.combatants.Wizard;
 import model.items.*;
 import model.status_effects.SmokeBombEffect;
 import model.turn_order.TurnOrderStrategy;
+//test
 
 public class GameController {
     private final Player player;
